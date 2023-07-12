672b64d9b9b14cce56b96535bfa95360b085230d
erennkazan@gmail.com <erennkazan@gmail.com>
2023-07-13 01:07:14 +0300

    json server created

---
+++ course/src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import {useEffect,useState} from 'react';
+import axios from 'axios';
+import Courses from './Courses'
+import Loading from './Loading';
+
+function App() {
+  const [courses, setCourses] = useState([])
+  const [loading, setLoading] = useState(true)
+  const fetchCourses = async ()=>{
+    setLoading(true);
+    try{
+      const response = await axios.get('http://localhost:3001/courses');
+      setCourses(response.data);
+      setLoading(false);
+    }
+    catch(error){
+      setLoading(false);
+    }
+  }
+
+  useEffect(()=>{
+    fetchCourses();
+  },[])
+  return (
+    <div className="App">
+      {loading ? (
+        <Loading/>
+      ):(
+        <Courses courses={courses}/>
+      )}
+    </div>
+  );
+}
+
+export default App;
