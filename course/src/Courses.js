5dc0beedcdfef3c9fc5e896c8fd95a501b7af73b
erennkazan@gmail.com <erennkazan@gmail.com>
2023-07-17 21:15:50 +0300

    usestate fixed

--- course/src/Courses.js
+++ course/src/Courses.js
@@ -1,6 +1,9 @@
 import Course from "./Course";
+import { useState } from "react";
 
 function Courses({courses,removeCourse}) {
+   const [index, setIndex] = useState(0);
+   const {content,title,price} =courses[index]
    
    return ( 
    <div className="courseMainDiv">
@@ -8,13 +11,21 @@ function Courses({courses,removeCourse}) {
         <h2>Kurslarım</h2>            
      </div>
         <div className="cardDiv">
+        <div className="card">
+        <div className="cardTitlePrice">
+          <h2 className="cardTitle">{title}</h2>
+          <h4 className="cardPrice">{price}TL</h4>
+        </div>
+        <p>{content}</p>
+        
+     </div> 
          {
-            courses.map((course)=>{
-               return(
-                    <Course  key={course.id} {...course} removeOneCourse= {removeCourse}/>
-                    // course={course}
-               )
-            })
+            // courses.map((course)=>{
+            //    return(
+            //         <Course  key={course.id} {...course} removeOneCourse= {removeCourse}/>
+            //         // course={course}
+            //    )
+            // })
          }
         </div>
    </div> );
