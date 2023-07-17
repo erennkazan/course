db2886dc95a472e01f971b7d795ddc012844f284
erennkazan@gmail.com <erennkazan@gmail.com>
2023-07-17 21:56:45 +0300

    randomcourse created

--- course/src/Courses.js
+++ course/src/Courses.js
@@ -1,16 +1,55 @@
 import Course from "./Course";
 import { useState } from "react";
+import {FaChevronLeft,FaChevronRight} from 'react-icons/fa'
 
 function Courses({courses,removeCourse}) {
    const [index, setIndex] = useState(0);
-   const {content,title,price} =courses[index]
+   const {content,title,price} = courses[index];
    
+   const checkIndex = (index)=>{
+      if(index<0){
+         return courses.length -1;
+      }
+      if(index >= courses.length - 1){
+         return 0;
+      }
+      return index;
+   };
+   
+   const getRandomCourse = ()=>{
+      let randomNumber = Math.floor(Math.random()*courses.length)
+      if(randomNumber === index){
+         randomNumber = index + 1;
+      }
+      setIndex(randomNumber);
+   }
+
+   const prevCourse = ()=>{
+      setIndex((index)=>{
+         let newIndex = index - 1;
+         return checkIndex(newIndex);
+      });
+   }
+   const nextCourse = ()=>{
+      setIndex((index)=>{
+         let newIndex = index + 1;
+         return checkIndex(newIndex);
+      });
+   }
+   
+
    return ( 
    <div className="courseMainDiv">
-     <div>
-        <h2>Kurslarım</h2>            
+     <div className="courseTitleAndBtn">
+        <h2>Kurslarım</h2>      
+        <button className="cardDeleteBtn" onClick={getRandomCourse}>
+           Rastgele Kurs Ata
+        </button>      
      </div>
         <div className="cardDiv">
+           <button className="prevNext" onClick={prevCourse}>
+            <FaChevronLeft/>
+           </button>
         <div className="card">
         <div className="cardTitlePrice">
           <h2 className="cardTitle">{title}</h2>
@@ -19,6 +58,9 @@ function Courses({courses,removeCourse}) {
         <p>{content}</p>
         
      </div> 
+          <button className="prevNext"  onClick={nextCourse}>
+            <FaChevronRight/>
+         </button>
          {
             // courses.map((course)=>{
             //    return(
